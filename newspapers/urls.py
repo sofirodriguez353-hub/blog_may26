94c4f906f8d4a777e8882387e54546e4ed400d3f
SOFIA RODRIGUEZ <sofirodriguez353@gmail.com>
2026-05-23 17:25:33 -0400

    post-detail is done

--- newspapers/urls.py
+++ newspapers/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import BlogListView
+from .views import BlogDetailView
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='post-list'),
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post-detail'),   
 ]
--- newspapers/views.py
+++ newspapers/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from .models import Blog
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 # Create your views here.
 class BlogListView(ListView):
     model = Blog
     template_name = 'post-list.html'
+
+class BlogDetailView(DetailView):
+    model = Blog
+    template_name = 'post-detail.html'
