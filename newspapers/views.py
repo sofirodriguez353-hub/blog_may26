f6c35c19a98455640c90922617e6f04dabd8c59d
SOFIA RODRIGUEZ <sofirodriguez353@gmail.com>
2026-05-23 16:48:57 -0400

    post-list is done

--- newspapers/views.py
+++ newspapers/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Blog
+from django.views.generic import ListView
 
 # Create your views here.
+class BlogListView(ListView):
+    model = Blog
+    template_name = 'post-list.html'
