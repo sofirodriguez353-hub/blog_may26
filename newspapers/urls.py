e944e13e929c2b6f515df1f9d7035e51b1055190
SOFIA RODRIGUEZ <sofirodriguez353@gmail.com>
2026-05-30 16:36:38 -0400

    create is done

--- newspapers/urls.py
+++ newspapers/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import BlogListView
 from .views import BlogDetailView
+from .views import BlogCreateView
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='post-list'),
-    path('post/<int:pk>/', BlogDetailView.as_view(), name='post-detail'),   
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post-detail'),
+    path('post/crear/', BlogCreateView.as_view(), name='post-crear'),
 ]
--- newspapers/views.py
+++ newspapers/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Blog
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView, ListView, CreateView
 
 # Create your views here.
 class BlogListView(ListView):
@@ -10,3 +10,8 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Blog
     template_name = 'post-detail.html'
+
+class BlogCreateView(CreateView):
+    model = Blog
+    template_name = 'post-create.html'
+    fields = ['title', 'content', 'author']
